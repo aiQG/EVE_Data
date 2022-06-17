89557e6edb050721164211b0ef32af8b7e1d8bb7
aiQG_ <zhouce.zjq@alibaba-inc.com>
2022-06-17 17:39:19 +0800

    first commit

---
+++ EVEKillDataGetter.py
@@ -0,0 +1,30 @@
+import json
+
+
+#    getUrl = "https://esi.evepc.163.com/latest/universe/constellations/" + str(id) + "/?datasource=serenity&language=zh"
+#    headers = {
+#    "Accept-Language": "zh",
+#    "accept": "application/json",
+#    "Cache-Control": "no-cache"
+#    }
+#
+#    req = requests.get(getUrl, headers=headers, timeout=10)
+##    print(req.status_code)
+#    assert(req.status_code==200)
+#    res_dic = req.json()
+
+
+#request = requests.session()
+#postUrl = "https://esi.evepc.163.com/latest/universe/names/?datasource=serenity"
+#headers = {
+#"Content-Type": "application/json",
+#"accept": "application/json",
+#"Cache-Control": "no-cache"
+#}
+#
+#    req = request.post(postUrl, data=json.dumps(id_list[min_index:min_index+1000]), headers=headers, timeout=10)
+#    print(req.status_code)
+#    assert(req.status_code==200)
+
+
+
